375968ff123e27f61af7b968698de947c643ce3c
Nikolai Mushegian <nikolai@nexusdev.us>
2017-08-27 12:08:29 -0400

    expose costfunction

---
+++ src/RIO.sol
@@ -0,0 +1,6 @@
+// public intellectualproperty units
+
+contract CostFunction {
+    // "have, want" from caller's POV
+    function cost(address have, address want, uint128 want_amount) returns (uint128 need_amount);
+}
